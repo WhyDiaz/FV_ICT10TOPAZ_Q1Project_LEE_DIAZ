69bc9b8cdcf08e00f3e4cc885a2a2b134f54d012
Lee <vaniluxvonn@gmail.com>
2026-09-17 16:15:02 +0800

    Rename function and update logic for adding numbers

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pyscript import document, display
 
-def adding_categories(e):
+def adding_numbers(e):
     document.getElementById('output1').innerHTML = "" # clears previous output
-    num1 = float(document.getElementById('input1').value) #get 1st input
-    num2 = float(document.getElementById('input2').value) #get 2nd input
-    result = num1 + num2 # use operator to compute
-    display(result, target= "output1") #diplay output in div
+    
+    categorybox = document.getElementById('Category').value # get 1st input
+    productname = document.getElementById('input2').value # get 2nd input
+    stockquantity = document.getElementById('input3').value # get 3rd input
+    
+    result =  categorybox + "-" + productname + "-" + stockquantity # use operator to combine all the values
+    display(result.upper(), target="output1") # display output in div
